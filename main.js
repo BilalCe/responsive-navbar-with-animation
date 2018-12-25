0226e56e9f8112375ab37ed9d509de9fe2e04c95
BilalCe <bilal.cehic91@gmail.com>
2018-12-25 18:46:45 +0100

    navabr

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".menu-toggle").click(function() {
+    $("nav").toggleClass("active");
+  });
+});
+
+$(window).on("scroll", function() {
+  if ($(window).scrollTop()) {
+    $("header").addClass("black");
+  } else {
+    $("header").removeClass("black");
+  }
+});
